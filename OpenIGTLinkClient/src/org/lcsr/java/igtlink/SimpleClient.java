f6a82145fd42a40e717ab4d39c0c5dfb058b656e
Qian Long <joewalker.ql@gmail.com>
2015-09-19 20:13:55 -0400

    A simple PC OpenIGTLink client to communicate with Moverio

---
+++ OpenIGTLinkClient/src/org/lcsr/java/igtlink/SimpleClient.java
@@ -0,0 +1,135 @@
+package org.lcsr.java.igtlink;
+
+import com.neuronrobotics.sdk.common.Log;
+import org.medcare.igtl.messages.ImageMessage;
+import org.medcare.igtl.messages.TransformMessage;
+import org.medcare.igtl.network.GenericIGTLinkClient;
+import org.medcare.igtl.network.IOpenIgtPacketListener;
+import org.medcare.igtl.util.Status;
+import Jama.Matrix;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+
+public class SimpleClient implements IOpenIgtPacketListener {
+	/**
+	 * @param args
+	 */
+	static boolean rx = false;
+	static int count = 0;
+	
+	public static void main(String[] args) {
+		String msg = "<Command Name=\"SomeCommandName\" SomeAttribute1=\"attribute value 1\" SomeAttribute2=\"123\"><Param name=\"Param1\"/><Param name=\"Param2\"/></Command>";
+
+		SimpleClient.parseXMLStringMessage(msg);
+		GenericIGTLinkClient client;
+		try {
+			Log.enableDebugPrint(true);
+			Log.enableSystemPrint(true);
+			
+			Log.debug("Starting client");
+			client = new GenericIGTLinkClient ("10.188.186.219",18944);
+			client.addIOpenIgtOnPacket(new SimpleClient());	
+			
+			for(int i=0;i<500;i++){
+				TransformNR t = new TransformNR();
+				double position[] = t.getPositionArray();
+				position[0] = count;
+				position[1] = count;
+				position[2] = (int)(Math.random()*100);
+				double rotation[][] = t.getRotationMatrixArray();
+				rotation[0][1] = Math.random();
+				count = count + 1;
+				client.sendMessage(new TransformMessage("PC_" + String.valueOf(count), position ,rotation ));
+				Thread.sleep(1000);
+			}
+			client.stopClient();
+			Log.debug("Client disconnected");
+			System.exit(0);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void onRxTransform(String name, TransformNR t) {
+		//Log.debug("Received Transform: "+t);  
+		if(name.equals("RegistrationTransform") || name.equals("CALIBRATION")){
+			System.err.println("Received Registration Transform");
+			Log.debug("Setting fiducial registration matrix: "+t); 
+			return;
+		}else if(name.equals("TARGET")){
+			System.err.println("Received RAS Transform: TARGET");
+			Log.debug("Setting task space pose: "+t); 
+			
+		}else if(name.equals("myTransform")){
+			System.err.println("Received Transformation Matrix: myTransform");
+			Log.debug("Setting task space pose: "+t); 
+			
+		}else{
+			System.err.println("Received unidentified transform matrix");
+			Log.debug("Setting task space pose: "+t); 
+		}
+	}
+
+	@Override
+	public TransformNR getTxTransform(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Status onGetStatus(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onRxString(String name, String body) {
+	}	
+
+	public static void parseXMLStringMessage(String msg){
+		// TODO Auto-generated method stub
+	}
+	
+
+	@Override
+	public String onTxString(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onRxDataArray(String name, Matrix data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double[] onTxDataArray(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onRxImage(String name, ImageMessage image) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTxNDArray(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRxNDArray(String name, float[] data) {
+		// TODO Auto-generated method stub
+		Log.debug("Name" + name);
+		for(int i=0;i<data.length;i++){
+			Log.debug("Data[" + i + "]=" + data[i]);
+		}
+	}
+}
